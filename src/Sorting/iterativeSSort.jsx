f8768fd5291b834eebc3609d868ac37a8983a736
Octaram <146319102+Octaram@users.noreply.github.com>
2025-12-27 18:28:13 +0700

    Update iterativeSSort.jsx

--- src/Sorting/iterativeSSort.jsx
+++ src/Sorting/iterativeSSort.jsx
@@ -2,19 +2,14 @@ export const iterativeSSort = (arr) => {
     let n = arr.length;
 
     for (let i = 0; i < n - 1; i++) {
-        // 1. Anggap elemen saat ini (i) adalah yang paling murah/kecil
         let minIndex = i;
 
-        // 2. Cari barang yang LEBIH MURAH di sisa array (j)
         for (let j = i + 1; j < n; j++) {
-            // Perhatikan: Kita membandingkan .harga (karena object)
-            // Ubah .harga menjadi .price jika data Anda menggunakan bahasa Inggris
             if (arr[j].harga < arr[minIndex].harga) {
-                minIndex = j; // Simpan indeksnya saja, JANGAN swap dulu
+                minIndex = j;
             }
         }
 
-        // 3. Jika ditemukan barang yang lebih murah dari posisi i, baru SWAP
         if (minIndex !== i) {
             let temp = arr[i];
             arr[i] = arr[minIndex];
